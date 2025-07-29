3db088d79c2b98fada3e5deb24857a99ab4c052a
Md. Imran Mia <mr.t.imran11@gmail.com>
2025-07-29 19:03:17 +0600

    added

--- project/src/main/java/com/emranhss/project/config/WebConfig.java
+++ project/src/main/java/com/emranhss/project/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.emranhss.project.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,5 +21,17 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:"+uploadDir);
 
 
+
     }
+
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:4200")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowCredentials(true);
+        }
+
+
 }
