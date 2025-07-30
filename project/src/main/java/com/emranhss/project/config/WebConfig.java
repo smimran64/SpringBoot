2b0ca2a70b2f4bd71753b668ba84c502f9cb2228
Md. Imran Mia <mr.t.imran11@gmail.com>
2025-07-30 12:37:52 +0600

    added

--- project/src/main/java/com/emranhss/project/config/WebConfig.java
+++ project/src/main/java/com/emranhss/project/config/WebConfig.java
@@ -25,13 +25,4 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
 
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE")
-                    .allowCredentials(true);
-        }
-
-
 }
